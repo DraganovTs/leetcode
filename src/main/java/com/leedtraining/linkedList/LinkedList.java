4f889fa4eddbe7e055babdc51be76a4a5bd435bc
DraganovTs <cvetomir_draganov@abv.bg>
2024-04-07 18:41:46 +0300

    linked list

--- src/main/java/com/leedtraining/linkedList/LinkedList.java
+++ src/main/java/com/leedtraining/linkedList/LinkedList.java
@@ -119,4 +119,74 @@ public class LinkedList {
         return temp;
     }
 
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+        if (temp != null) {
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean insert(int index, int value) {
+        if (index < 0 || index > length) return false;
+        if (index == 0) {
+            prepend(value);
+            return true;
+        }
+        if (index == length) {
+            append(value);
+            return true;
+        }
+        Node newNode = new Node(value);
+        Node temp = get(index - 1);
+        newNode.next = temp.next;
+        temp.next = newNode;
+        length++;
+        return true;
+    }
+
+    public Node remove(int index) {
+        if (index < 0 || index >= length) return null;
+
+        if (index == 0) return removeFirst();
+        if (index == length - 1) return removeLast();
+
+        Node prev = get(index - 1);
+        Node temp = prev.next;
+        prev.next = temp.next;
+        temp.next = null;
+        length--;
+        return temp;
+
+    }
+
+    public void reverse() {
+        Node temp = head;
+        head = tail;
+        tail = temp;
+        Node before = null;
+        Node after = temp.next;
+        for (int i = 0; i < length; i++) {
+            after = temp.next;
+            temp.next = before;
+            before = temp;
+            temp = after;
+        }
+    }
+
+    public boolean hasLoop() {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null || fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
--- src/main/java/com/leedtraining/linkedList/Main.java
+++ src/main/java/com/leedtraining/linkedList/Main.java
@@ -6,22 +6,37 @@ public class Main {
     public static void main(String[] args) {
 
 
-        LinkedList myLinkedList = new LinkedList(0);
-        myLinkedList.append(1);
+        LinkedList myLinkedList = new LinkedList(1);
         myLinkedList.append(2);
         myLinkedList.append(3);
+        myLinkedList.append(4);
 
+        System.out.println("LL before reverse():");
+        myLinkedList.printList();
 
-        System.out.println(myLinkedList.get(3).value);
+        myLinkedList.reverse();
+
+        System.out.println("\nLL after reverse():");
+        myLinkedList.printList();
 
 
         /*
             EXPECTED OUTPUT:
             ----------------
+            LL before reverse():
+            1
+            2
             3
+            4
+
+            LL after reverse():
+            4
+            3
+            2
+            1
 
         */
 
-
     }
+
 }
