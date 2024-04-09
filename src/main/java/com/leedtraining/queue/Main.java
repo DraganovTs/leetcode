84822e15e6749b75c9b23e8d8ae3195d175279ac
DraganovTs <cvetomir_draganov@abv.bg>
2024-04-09 20:44:06 +0300

    Queue

--- src/main/java/com/leedtraining/queue/Main.java
+++ src/main/java/com/leedtraining/queue/Main.java
@@ -3,5 +3,25 @@ package com.leedtraining.queue;
 public class Main {
     public static void main(String[] args) {
 
+        Queue myQueue = new Queue(2);
+        myQueue.enqueue(1);
+
+        // (2) Items - Returns 2 Node
+        System.out.println(myQueue.dequeue().value);
+        // (1) Item - Returns 1 Node
+        System.out.println(myQueue.dequeue().value);
+        // (0) Items - Returns null
+        System.out.println(myQueue.dequeue());
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            2
+            1
+            null
+
+        */
+
     }
 }
