45584a95cf07d44b10932786976704413fb77c64
DraganovTs <cvetomir_draganov@abv.bg>
2024-04-12 19:18:59 +0300

    Graph

--- src/main/java/com/leedtraining/hashTable/Main.java
+++ src/main/java/com/leedtraining/hashTable/Main.java
@@ -1,9 +1,6 @@
 package com.leedtraining.hashTable;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
@@ -57,10 +54,66 @@ public class Main {
         return null;
     }
 
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int pairNum = target - num;
+            if (numMap.containsKey(pairNum)) {
+                return new int[]{numMap.get(pairNum), i};
+            }
+            numMap.put(num, i);
+
+
+        }
+
+
+        return new int[]{};
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strings) {
+        Map<char[], List<String>> hashMap = new HashMap<>();
+
+        for (int i = 0; i < strings.length; i++) {
+            char[] key = strings[i].toCharArray();
+            Arrays.sort(key);
+
+            if (hashMap.containsKey(key)) {
+                hashMap.get(key).add(strings[i]);
+            } else {
+                List<String> group = new ArrayList<>();
+                group.add(strings[i]);
+                hashMap.put(key, group);
+            }
+        }
+
+        return new ArrayList<>(hashMap.values());
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(firstNonRepeatingChar("leetcode"));
-        System.out.println(firstNonRepeatingChar("hello"));
-        System.out.println(firstNonRepeatingChar("aabbcc"));
+        System.out.println("1st set:");
+        System.out.println(groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+
+        System.out.println("\n2nd set:");
+        System.out.println(groupAnagrams(new String[]{"abc", "cba", "bac", "foo", "bar"}));
+
+        System.out.println("\n3rd set:");
+        System.out.println(groupAnagrams(new String[]{"listen", "silent", "triangle", "integral", "garden", "ranged"}));
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            1st set:
+            [[eat, tea, ate], [tan, nat], [bat]]
+
+            2nd set:
+            [[abc, cba, bac], [foo], [bar]]
+
+            3rd set:
+            [[listen, silent], [triangle, integral], [garden, ranged]]
+
+        */
     }
 }
